5ca6585b729dd6beeb386cf434ca6b6e6432b2cf
JonathanMov <jafetmv21@gmail.com>
2022-02-15 09:17:15 -0800

    Panel Section Completed

---
+++ updater.js
@@ -0,0 +1,42 @@
+const changePassword = document.querySelector('#changePassword');
+const changeEmail = document.querySelector('#changeEmail');
+const container = document.querySelector('.updater__container');
+
+const passwordInfo = {
+    title: 'Cambiar Clave',
+    description: 'Aquí cambia tu contraseña por una que te sea más fácil recordar.',
+    placeholder1: 'Clave nueva',
+    placeholder2: 'Confirmar clave',
+    label: 'Por favor escribe la clave nueva de 6 a 10 caracteres.'
+}
+
+const emailInfo = {
+    title: 'Actualizar correo',
+    description: 'Debes actualizar tu correo para que puedas realizar el proceso de reinscripción.',
+    placeholder1: 'Correo nuevo',
+    placeholder2: 'Confirmar correo'
+}
+
+changePassword.addEventListener('click', e => {
+    container.classList.toggle('updater--visible');
+    changeTemplate(passwordInfo);
+});
+
+changeEmail.addEventListener('click', e => {
+    container.classList.toggle('updater--visible');
+    changeTemplate(emailInfo);
+});
+
+container.addEventListener('click', e => {
+    console.log(e);
+    if (e.target.classList.contains('updater__container'))
+        container.classList.toggle('updater--visible');
+})
+
+const changeTemplate = (info) => {
+    container.querySelector('.updater-title').textContent = info.title;
+    container.querySelector('.updater-description').textContent = info.description;
+    container.querySelector('#newFieldInput').placeholder = info.placeholder1;
+    container.querySelector('#confirmFieldInput').placeholder = info.placeholder2;
+    container.querySelectorAll('.input-label').textContent = info.label ? info.label : null;
+}
